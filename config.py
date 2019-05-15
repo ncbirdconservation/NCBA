42bfce82e64a7600cc7af68c64ac16bd66450c6b
Nathan Tarr <nmtarr@ncsu.edu>
2019-05-15 16:27:15 -0400

    Madtom notebook.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #NOTE! this is overwritten by some notebooks, so update everywhere, if adding lines.
-sp_id = 'bsespm0'
-summary_name = 'seaside'
-gbif_req_id = 'GBIFr10'
+sp_id = 'fcamax0'
+summary_name = 'madtom'
+gbif_req_id = 'GBIFr8'
 gbif_filter_id = 'GBIFf5'
 ebird_req_id = None
 ebird_filter_id = None
@@ -10,6 +10,6 @@ workDir = '/Users/nmtarr/Documents/RANGES/'
 codeDir = '/Users/nmtarr/Code/range_map_evaluation/'
 inDir = workDir + 'Inputs/'
 outDir = workDir + 'Outputs/'
-default_coordUncertainty = 200
+default_coordUncertainty = 500
 SRID_dict = {'WGS84': 4326, 'AlbersNAD83': 102008} # Used in file names for output.
 spdb = outDir + sp_id + gbif_req_id + gbif_filter_id + '.sqlite'
