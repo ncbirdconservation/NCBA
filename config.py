5db0d877c07dc2480838e344d2b29adb4ac5dd49
Nathan Tarr <nmtarr@ncsu.edu>
2019-05-16 13:58:32 -0400

    Updated a bYBCUx0 notebook.

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #NOTE! this is overwritten by some notebooks, so update everywhere, if adding lines.
-sp_id = 'fcamax0'
-summary_name = 'madtom'
-gbif_req_id = 'GBIFr8'
-gbif_filter_id = 'GBIFf5'
+sp_id = 'bybcux0'
+summary_name = 'cuckoo'
+gbif_req_id = 'GBIFr7'
+gbif_filter_id = 'GBIFf6'
 ebird_req_id = None
 ebird_filter_id = None
 evaluation = 'eval_gbif1'
@@ -10,6 +10,6 @@ workDir = '/Users/nmtarr/Documents/RANGES/'
 codeDir = '/Users/nmtarr/Code/range_map_evaluation/'
 inDir = workDir + 'Inputs/'
 outDir = workDir + 'Outputs/'
-default_coordUncertainty = 500
+default_coordUncertainty = 100
 SRID_dict = {'WGS84': 4326, 'AlbersNAD83': 102008} # Used in file names for output.
 spdb = outDir + sp_id + gbif_req_id + gbif_filter_id + '.sqlite'
