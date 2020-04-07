d4628dd7fe7bff66d28bbb0786133761017bf761
Nathan Tarr <nmtarr@ncsu.edu>
2020-04-07 14:22:04 -0400

    Changes format of data in ebirdst season csv.

--- get_ebird_seasons_20200403NT.R
+++ get_ebird_seasons_20200403NT.R
@@ -37,9 +37,33 @@ select("NCBA_common_name", "breeding_start_dt", "breeding_end_dt",
 "prebreeding_migration_end_dt", "year_round_start_dt",
 "year_round_end_dt") %>%
 # Sort
-arrange(NCBA_common_name)
+arrange(NCBA_common_name) #%>%
 
-# Drop year from season date values
-seasons
+# Change the format of the data to be more readable
+# Create a function to convert dates into characters like "April-14"
+month_day <- function(x) {
+    month_name <- toString(month(x, label=TRUE))
+    day_char <- toString(day(x))
+    out <- str_c(month_name, day_char, sep="-")
+  return(out)
+  }
 
-View(seasons)
+# Apply the function to date values to reformat them.
+columns <- colnames(seasons)
+columns <- columns[columns != "NCBA_common_name"]
+for (col in columns) {
+  col2 <- pull(seasons, col)
+  seasons[col] <- map_chr(col2, month_day)
+}
+
+# Capitalize common names appropriately
+seasons2 <- seasons %>% mutate(
+  NCBA_common_name = str_replace(str_to_title(seasons$NCBA_common_name),
+                                 "-.",
+                                 str_to_lower(str_extract(seasons$NCBA_common_name,
+                                                          "-."))))
+
+# Save
+write_csv(seasons2, "T:/NCBA/Preliminary/ebirdst_seasons_2018.csv")
+View(seasons2)
+#                          source("T:/code/ncba/get_ebird_seasons_20200403NT.R")
