5007a8e6d2b2fb768e4f9a5599aa8c5fd23e5dc3
Pearson, Scott <pearsonsm@ornl.gov>
2023-11-29 13:30:38 -0500

    plot_dev_smp.R

---
+++ ncba_config_smp.r
@@ -0,0 +1,21 @@
+# Login info for MongoDB instance
+
+USER <- "ncba_scottp"
+PASS <- "setophagacitrina"
+
+ncba_db_user <- "ncba_scottp"
+ncba_db_pass <- "setophagacitrina"
+
+# Working directory
+# work_dir <- "~/Documents/NCBA/Analyses/"
+work_dir <- "C:/Users/spearson/Dropbox/NCBA/code/NCBA/shinyapp"
+
+# Code repo
+# code_dir <- "C:/Users/skanderson/OneDrive - State of North Carolina/@@ncba/ncba"
+code_dir <- "C:/Users/spearson/Dropbox/NCBA/code/NCBA"
+
+# EBird Basic Dataset Copy
+EBD_observations <- "/Volumes/NMTARR1/Datasets/EBD/ebd_US-NC_202101_202312_smp_relJul-2023.txt"
+# EBird Sampling Event Dataset path
+EBD_sampling <- "/Volumes/NMTARR1/Datasets/EBD/ebd_US-NC_202101_202312_smp_relJul-2023_sampling.txt"
+
